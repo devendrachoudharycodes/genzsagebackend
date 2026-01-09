95e723c6481ccbd74721e7efdce2f7f267391848
devendrachoudharycodes <devendra.choudhary.codes@gmail.com>
2026-01-09 20:11:02 +0530

    Initial commit

---
+++ src/test/java/com/genzsage/genzsage/GenzsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genzsage.genzsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenzsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
